50ef54beafb154718c9e71d3cfc55e09febe48ff
Mark Levy <levymark@gmail.com>
2024-11-01 00:44:09 +0200

    add save button to news card

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 import "./NewsCard.css";
 import { IsLoggedInContext } from "../../contexts/IsLoggedInContext";
+import { CurrentPageContext } from "../../contexts/CurrentPageContext";
 
+//////////////////////////Temporary ////////////////////////
 import { defaultNewsItems } from "../../utils/constants";
 import image1 from "../../assets/temp/image_01.png";
 import image2 from "../../assets/temp/image_02.png";
 import image3 from "../../assets/temp/image_03.png";
 import image4 from "../../assets/temp/image_04.png";
 import image5 from "../../assets/temp/image_05.png";
+////////////////////////////////////////////////////////////
 
 function NewsCard() {
   const isLoggedIn = useContext(IsLoggedInContext);
+  const currentPage = useContext(CurrentPageContext);
 
   const card = defaultNewsItems[0];
   const images = [image1, image2, image3, image4, image5];
@@ -19,16 +23,26 @@ function NewsCard() {
   return (
     <div className="card">
       <div className="card__header">
-        {isLoggedIn ? "" : ""}
-        <p className="card__keyword card__keyword-visible">{card.keyword}</p>
-        <p className="card__signin-prompt card__signin-prompt-visible">
-          Sign in to save articles
-        </p>
-        <button className="card__save_btn"></button>
-        <p className="card__delete-prompt card__delete-prompt-visible">
-          Remove from saved
-        </p>
-        <button className="card__delete_btn"></button>
+        {isLoggedIn ? (
+          currentPage === "home" ? (
+            <button className="card__save-btn_unchecked"></button>
+          ) : (
+            <>
+              <p className="card__keyword">{card.keyword}</p>
+              <p className="card__delete-prompt card__delete-prompt-visible">
+                Remove from saved
+              </p>
+              <button className="card__delete_btn"></button>
+            </>
+          )
+        ) : (
+          <>
+            <p className="card__signin-prompt card__signin-prompt-visible">
+              Sign in to save articles
+            </p>
+            <button className="card__save-btn_unchecked"></button>
+          </>
+        )}
       </div>
       <div className="card__content">
         <img className="card__image" src={images[card._id]} alt={card.title} />
