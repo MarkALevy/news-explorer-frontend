70b393617a885c22b1e524fd0831cee8ea97f234
Mark Levy <levymark@gmail.com>
2024-10-31 16:23:58 +0200

    create preloader

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,13 @@ import "./Main.css";
 import SearchResults from "../SearchResults/SearchResults";
 import About from "../About/About";
 import NothingFound from "../NothingFound/NothingFound";
+import Preloader from "../Preloader/Preloader";
 function Main() {
   return (
     <main>
       {/* <SearchResults /> */}
-      <NothingFound />
+      {/* <NothingFound /> */}
+      <Preloader />
       <About />
     </main>
   );
