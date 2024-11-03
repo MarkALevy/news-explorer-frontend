88e8150082f73d8aa2732337b1754a43f983a2b2
Mark Levy <levymark@gmail.com>
2024-11-03 17:42:25 +0200

    responsive style for mobile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,7 +23,7 @@ import "./App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [activeModal, setActiveModal] = useState("register");
+  const [activeModal, setActiveModal] = useState("success");
   const [currentPage, setCurrentPage] = useState("home");
   const [likedItems, setLikedItems] = useState(
     defaultNewsItems.filter((item) => {
